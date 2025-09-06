d4b368aa76697b4ddaa2114bf3cb641b81f3bd89
flopie <dehaan@dataprovider.com>
2025-09-06 15:00:38 +0200

    wip

--- resept-frontend/src/App.tsx
+++ resept-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Menu } from "./components/Menu";
 import { Recipe } from "./components/Recipe";
