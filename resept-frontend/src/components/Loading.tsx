0157f5729e4b1a703e43f4d87df6bc3a59668adf
flopie <dehaan@dataprovider.com>
2025-11-20 15:32:27 +0100

    delay loading component

--- resept-frontend/src/components/Loading.tsx
+++ resept-frontend/src/components/Loading.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import garlicImage from "../assets/garlic.png";
 
 export const Loading = () => {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const delayTimer = setTimeout(() => {
+      setVisible(true);
+    }, 200);
+    return () => {
+      clearTimeout(delayTimer);
+    };
+  }, []);
+
   return (
-    <div className="flex w-full h-full justify-center items-center">
+    <div
+      className="flex w-full h-full justify-center items-center transition-opacity duration-[300ms]"
+      style={{ opacity: visible ? 1 : 0 }}
+    >
       <img
         src={garlicImage}
         alt="Loading..."
